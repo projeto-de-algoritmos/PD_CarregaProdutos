1d300458462affeeb90fb72a8d588f86c4e99653
João Soares <jpcirqueira81@gmail.com>
2020-12-03 11:20:03 -0300

    Adicionado logica para preencher lista itens.
    Signed-off-by: João Soares <jpcirqueira81@gmail.com>
    Co-authored-by: lucasA27 <lucas.alexandre.df@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+itens = []
+listaitens = []
 def knapSack(W, wt, val, n): 
     K = [[0 for x in range(W + 1)] for x in range(n + 1)] 
 
@@ -11,13 +13,35 @@ def knapSack(W, wt, val, n):
                               K[i-1][w]) 
             else: 
                 K[i][w] = K[i-1][w] 
-  
-    return K[n][W] 
-  
-  
-# Driver code 
-val = [60, 100, 120] 
-wt = [10, 20, 30] 
-W = 50
-n = len(val) 
-print(knapSack(W, wt, val, n)) 
+    
+    print(K[n][W])
+    return K
+
+while(1):
+    nomeproduto = str(input("digite o nome do produto.\n"))
+    if nomeproduto == '':
+        break
+    valorproduto = int(input("digite o valor do produto.\n"))
+    pesoproduto = int(input("digite o peso do produto.\n"))
+
+    listaitens.append([nomeproduto,valorproduto,pesoproduto])
+
+W = int(input("digite a capacidade de carregamento do.\n"))
+# val = [60, 100, 120] 
+# wt = [10, 20, 30] 
+# W = 50
+
+itens = knapSack(W, wt, val, n)
+print(listaitens)
+# itensLevados = []
+# estahNoCaminhao = False
+# for linha in range(len(val), 0, -1):
+#         if(itens[linha][W] != itens[linha-1][W]):
+#             estahNoCaminhao = True
+        
+#         if estahNoCaminhao:
+#             wt = val[linha-1]
+#             itensLevados.append(val[linha-1])
+#             W -= wt
+
+##print(itensLevados)
